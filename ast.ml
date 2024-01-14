fc2d2e31a8ea57c8dd8389524bd885ded75669ab
Ev <adam.elgerssifi@gmail.com>
2024-01-14 19:12:36 +0100

    ajout du début de la transformation en C

--- ast.ml
+++ ast.ml
@@ -19,40 +19,48 @@ let rec print_mot_liste l =
   | _ -> raise PhraseInvalide
 
 
-let rec affiche a =
-  match a with
-  | Phrase(l, p) ->
-    print_string "Phrase(";
-    print_mot_liste l;
-    Printf.printf ", Ponctuation(%s)" p;
-    print_string ")\n"
-  | Plus(p1, p2) ->
-    print_string "(";
-    affiche p1;
-    print_string " + ";
-    affiche p2;
-    print_string ")"
-  | Fois(p1, p2) ->
-    print_string "(";
-    affiche p1;
-    print_string " * ";
-    affiche p2;
-    print_string ")"
-  | Egal(p1, p2) -> 
-    print_string "(";
-    affiche p1;
-    print_string " = ";
-    affiche p2;
-    print_string ")"
-  | Nombre(d) -> print_string d
-  | Mot(m) -> print_string (String.lowercase_ascii m)
-  | Assigne(m, e) ->
-    affiche m;
-    print_string " = ";
-    affiche e;
-    print_string ";"
-  | Paragraphe(l) -> 
-    print_string "Paragraphe(";
-    List.iter affiche l;
-    print_string ")"
-  | _ -> raise TokenInvalide
+let affiche a =
+
+  let rec affiche_aux a =
+    match a with
+    | Phrase(l, p) ->
+      print_string "Phrase(";
+      print_mot_liste l;
+      Printf.printf ", Ponctuation(%s)" p;
+      print_string ")\n"
+    | Plus(p1, p2) ->
+      print_string "(";
+      affiche_aux p1;
+      print_string " + ";
+      affiche_aux p2;
+      print_string ")"
+    | Fois(p1, p2) ->
+      print_string "(";
+      affiche_aux p1;
+      print_string " * ";
+      affiche_aux p2;
+      print_string ")"
+    | Egal(p1, p2) -> 
+      print_string "(";
+      affiche_aux p1;
+      print_string " == ";
+      affiche_aux p2;
+      print_string ")"
+    | Nombre(d) -> print_string d
+    | Mot(m) -> print_string "int "; print_string (String.lowercase_ascii m)
+    | Assigne(m, e) ->
+      affiche_aux m;
+      print_string " = ";
+      affiche_aux e;
+      print_string ";"
+    | Paragraphe(l) -> 
+      print_string "Paragraphe(";
+      List.iter affiche_aux l;
+      print_string ")"
+    | _ -> raise TokenInvalide
+
+    in 
+
+    print_string "#include <stdio.h>\n\nint main(){\n";
+    affiche_aux a;
+    print_string "\nprintf(\"%d\\n\", coucou);\nreturn 0;\n}"
