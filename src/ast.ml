c338250e8084a0e4273c078c3336912417467064
Quentin PEPE <quentin.pepe@ensiie.eu>
2024-01-15 03:27:37 +0100

    Ajout d'un scope

--- src/ast.ml
+++ src/ast.ml
@@ -52,74 +52,99 @@ let rec contient_afficher a =
            false ast_list
   | _ -> false
 
-let affiche a =
-  let rec affiche_aux a =
-    match a with
-    | Phrase (l, p) ->
-        print_string "Phrase(";
-        print_mot_liste l;
-        Printf.printf ", Ponctuation(%s)" p;
-        print_string ")\n"
-    | Plus (p1, p2) ->
-        print_string "(";
-        affiche_aux p1;
-        print_string " + ";
-        affiche_aux p2;
-        print_string ")"
-    | Fois (p1, p2) ->
-        print_string "(";
-        affiche_aux p1;
-        print_string " * ";
-        affiche_aux p2;
-        print_string ")"
-    | Egal (p1, p2) ->
-        print_string "(";
-        affiche_aux p1;
-        print_string " == ";
-        affiche_aux p2;
-        print_string ")"
-    | Nombre d -> print_string d
-    | Mot m -> print_string (String.lowercase_ascii m)
-    | Assigne (m, e) ->
-        print_string "int ";
-        affiche_aux m;
-        print_string " = ";
-        affiche_aux e;
-        print_string ";\n"
-    | Paragraphe l -> List.iter affiche_aux l
-    | Afficher s ->
-        print_string "printf(\"%d\\n\", ";
-        affiche_aux s;
-        print_string ");\n"
-    | Condition (cond, alors_list, sinon_paragraphe_opt) -> (
-        Printf.printf "if (";
-        affiche_aux cond;
-        Printf.printf ") {\n";
-        List.iter affiche_aux alors_list;
-        Printf.printf "}\n";
-        match sinon_paragraphe_opt with
-        | Some sinon_list ->
-            Printf.printf "else {\n";
-            List.iter affiche_aux sinon_list;
-            Printf.printf "}\n"
-        | None -> ())
-    | Expression expr -> affiche_aux expr
-    | BoucleTantQue (cond, paragraphe) ->
-        Printf.printf "while (";
-        affiche_aux cond;
-        Printf.printf ") {\n";
-        List.iter affiche_aux paragraphe;
-        Printf.printf "}\n"
-    | Different (p1, p2) ->
-        print_string "(";
-        affiche_aux p1;
-        print_string " != ";
-        affiche_aux p2;
-        print_string ")"
-    | _ -> raise TokenInvalide
-  in
+type scope = (string * string) list
 
+let variable_est_declaree scope var_name =
+  List.exists (fun (name, _) -> name = var_name) scope
+
+let rec affiche_aux scope a =
+  match a with
+  | Phrase (l, p) ->
+      print_string "Phrase(";
+      print_string ")\n";
+      scope
+  | Plus (p1, p2) ->
+      print_string "(";
+      affiche_aux scope p1;
+      print_string " + ";
+      affiche_aux scope p2;
+      print_string ")";
+      scope
+  | Fois (p1, p2) ->
+      print_string "(";
+      affiche_aux scope p1;
+      print_string " * ";
+      affiche_aux scope p2;
+      print_string ")";
+      scope
+  | Egal (p1, p2) ->
+      print_string "(";
+      affiche_aux scope p1;
+      print_string " == ";
+      affiche_aux scope p2;
+      print_string ")";
+      scope
+  | Nombre d ->
+      print_string d;
+      scope
+  | Mot m ->
+      print_string (String.lowercase_ascii m);
+      scope
+  | Assigne (Mot m, e) ->
+      let doit_declarer = not (variable_est_declaree scope m) in
+      let new_scope = if doit_declarer then (m, "int") :: scope else scope in
+      if doit_declarer then print_string "int ";
+      let _ = affiche_aux new_scope (Mot m) in
+      print_string " = ";
+      let _ = affiche_aux new_scope e in
+      print_string ";\n";
+      new_scope
+  | Paragraphe l ->
+      List.fold_left (fun acc_scope expr -> affiche_aux acc_scope expr) scope l
+  | Afficher s ->
+      print_string "printf(\"%d\\n\", ";
+      let _ = affiche_aux scope s in
+      print_string ");\n";
+      scope
+  | Condition (cond, alors_list, sinon_paragraphe_opt) -> (
+      Printf.printf "if (";
+      let _ = affiche_aux scope cond in
+      Printf.printf ") {\n";
+      let new_scope = List.fold_left affiche_aux scope alors_list in
+      Printf.printf "}\n";
+      match sinon_paragraphe_opt with
+      | Some sinon_list ->
+          Printf.printf "else {\n";
+          let new_scope = List.fold_left affiche_aux new_scope sinon_list in
+          Printf.printf "}\n";
+          new_scope
+      | None -> new_scope)
+  | BoucleTantQue (cond, paragraphe) ->
+      Printf.printf "while (";
+      affiche_aux scope cond;
+      Printf.printf ") {\n";
+      let new_scope = List.fold_left affiche_aux scope paragraphe in
+      Printf.printf "}\n";
+      new_scope
+  | Different (p1, p2) ->
+      print_string "(";
+      affiche_aux scope p1;
+      print_string " != ";
+      affiche_aux scope p2;
+      print_string ")";
+      scope
+  | _ -> scope (* pour les autres cas *)
+
+let affiche a =
+  let rec affiche_main scope a =
+    match a with
+    | Paragraphe l ->
+        List.fold_left
+          (fun acc_scope expr -> affiche_main acc_scope expr)
+          scope l
+    | _ -> affiche_aux scope a
+  in
   if contient_afficher a then print_endline "#include <stdio.h>\n";
   print_string "\nint main(){\n";
-  affiche_aux a;
+  let _ = affiche_main [] a in
   print_string "\nreturn 0;\n}"
