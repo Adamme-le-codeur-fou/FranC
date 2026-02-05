25b293c410d98b457725b664d658f2e3c93bdb73
Adamme <adam.elgerssifi@gmail.com>
2026-02-05 11:33:02 +0100

    ajout d'un fonction pour afficher la portée

--- lib/analyse/portee.ml
+++ lib/analyse/portee.ml
@@ -4,3 +4,7 @@ type portee = (string * type_expression) list
 
 let variable_est_declaree portee var_name =
   List.exists (fun (name, _) -> name = var_name) portee
+
+let debug_portee portee type_vers_string =
+  let string_list = List.map (fun (nom, t) -> Printf.sprintf "(%s,%s)" nom (type_vers_string t)) portee in
+  Printf.printf "[%s]\n" (String.concat "; " string_list)
