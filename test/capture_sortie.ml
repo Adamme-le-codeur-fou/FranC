e4c9f95534eb9630d09f1f1a1690c5c94093a075
Adamme <adam.elgerssifi@gmail.com>
2026-02-06 12:30:03 +0100

    ajout d'un fichier utilitaire pour capturer la sortie d'une fonction

---
+++ test/capture_sortie.ml
@@ -0,0 +1,15 @@
+open Unix
+open FranC.Ecrire
+
+let capture_sortie_avec fonction =
+  let lecture, ecriture = pipe () in
+  let canal_entree = in_channel_of_descr lecture in
+  let canal_sortie = out_channel_of_descr ecriture in
+
+  change_canal canal_sortie;
+  fonction ();
+  close_out canal_sortie;
+
+  let result = really_input_string canal_entree (in_channel_length canal_entree) in
+  close_in canal_entree;
+  result
